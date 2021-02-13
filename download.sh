638a8cf533fb037a776266d7fc6f39d39cf89278
Lonnie Liu <h8liu@shanhu.io>
2021-02-13 03:25:38 +0000

    incorperate os-initrd-base

--- download.sh
+++ download.sh
@@ -16,11 +16,13 @@ curl -L 'https://storage.googleapis.com/golang/go1.8.5.linux-amd64.tar.gz' -o '_
 curl -L 'https://storage.googleapis.com/golang/go1.15.8.linux-amd64.tar.gz' -o '_/go1.15.8.linux-amd64.tar.gz'
 
 # docker client
-
 curl -L 'https://get.docker.com/builds/Linux/x86_64/docker-1.10.3' -o '_/docker-amd64'
 curl -L 'https://github.com/burmilla/docker/releases/download/v1.10.3-ros1/docker-1.10.3_arm64' -o '_/docker-arm64'
 
 # os-base
-
 curl -L 'https://github.com/burmilla/os-base/releases/download/v2018.02.11-1/os-base_amd64.tar.xz' -o '_/os-base_amd64.tar.xz'
 curl -L 'https://github.com/burmilla/os-base/releases/download/v2018.02.11-1/os-base_arm64.tar.xz' -o '_/os-base_arm64.tar.xz'
+
+# os-initrd-base
+curl -L 'https://github.com/burmilla/os-initrd-base/releases/download/v2018.02.11-1/os-initrd-base-amd64.tar.gz' -o '_/os-initrd-base-amd64.tar.gz'
+curl -L 'https://github.com/burmilla/os-initrd-base/releases/download/v2018.02.11-1/os-initrd-base-arm64.tar.gz' -o '_/os-initrd-base-arm64.tar.gz'
