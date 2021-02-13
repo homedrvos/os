7b52c642332b966bfba0ec71916c613a4510c7ca
Lonnie Liu <h8liu@shanhu.io>
2021-02-13 02:09:51 +0000

    fix lints

--- download.sh
+++ download.sh
@@ -2,8 +2,9 @@
 
 set -ex
 
-mkdir -p bits
-curl -L 'https://github.com/burmilla/refpolicy/releases/download/v0.0.3/policy.29' -o '_bits/selinux_policy.29'
-curl -L 'https://github.com/burmilla/os-kernel/releases/download/v4.14.218-burmilla/linux-4.14.218-burmilla-x86.tar.gz' -o '_bits/linux-4.14.218-burmilla-x86.tar.gz'
-curl -L 'https://storage.googleapis.com/golang/go1.8.5.linux-amd64.tar.gz' -o '_bits/go1.8.5.linux-amd64.tar.gz'
+mkdir -p _
+curl -L 'https://github.com/burmilla/refpolicy/releases/download/v0.0.3/policy.29' -o '_/selinux_policy.29'
+curl -L 'https://github.com/burmilla/os-kernel/releases/download/v4.14.218-burmilla/linux-4.14.218-burmilla-x86.tar.gz' -o '_/linux-4.14.218-burmilla-x86.tar.gz'
+curl -L 'https://storage.googleapis.com/golang/go1.8.5.linux-amd64.tar.gz' -o '_/go1.8.5.linux-amd64.tar.gz'
+curl -L 'https://storage.googleapis.com/golang/go1.15.8.linux-amd64.tar.gz' -o '_/go1.15.8.linux-amd64.tar.gz'
 # curl -L 'https://github.com/burmilla/os-kernel/releases/download/v4.14.218-burmilla/linux-4.14.218-burmilla-arm64.tar.gz' -o linux-4.14.218-burmilla-arm64.tar.gz
